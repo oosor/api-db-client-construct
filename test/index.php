b647b0d6353eee237ce1475936f61306db4e3615
oosor <3oosor@gmail.com>
2019-07-19 15:02:27 +0300

    test

--- test/index.php
+++ test/index.php
@@ -14,24 +14,19 @@ $updateTableName = new \Oosor\ClientConstruct\UpdateBuilder('table_update_name')
 
 $insert->addColumn('col_name_1', function (\Oosor\ClientConstruct\Models\Build $build) {
     $build->bigIncrements();
-});
-$insert->addColumn('col_name_2', function (\Oosor\ClientConstruct\Models\Build $build) {
+})->addColumn('col_name_2', function (\Oosor\ClientConstruct\Models\Build $build) {
     $build->string(255)->default('default value');
-});
-$insert->addColumn('col_name_3', function (\Oosor\ClientConstruct\Models\Build $build) {
+})->addColumn('col_name_3', function (\Oosor\ClientConstruct\Models\Build $build) {
     $build->date()->nullable();
 });
 
 $update->addColumn('col_name_4', function (\Oosor\ClientConstruct\Models\Build $build) {
     $build->json()->pushPatch();
-});
-$update->addColumn('col_name_5', function (\Oosor\ClientConstruct\Models\Build $build) {
+})->addColumn('col_name_5', function (\Oosor\ClientConstruct\Models\Build $build) {
     $build->date()->default('2019-07-12')->changePatch();
-});
-$update->addColumn('col_name_2', function (\Oosor\ClientConstruct\Models\Build $build) {
+})->addColumn('col_name_2', function (\Oosor\ClientConstruct\Models\Build $build) {
     $build->string(255)->dropPatch();
-});
-$update->addColumn('col_name_3', function (\Oosor\ClientConstruct\Models\Build $build) {
+})->addColumn('col_name_3', function (\Oosor\ClientConstruct\Models\Build $build) {
     $build->date(255)->renamePatch('col_new_name_3');
 });
 
